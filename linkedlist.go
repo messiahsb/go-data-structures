b689e6747a842e7fdad9d66198bb688e5cebea29
messiahsb <messiahs@buffalo.edu>
2024-04-16 15:50:36 -0400

    Implemented Add Funtion and Print function for the list

--- linkedlist.go
+++ linkedlist.go
@@ -7,7 +7,17 @@ type Node struct {
 	next *Node
 }
 
-func addNode() {
+var head *Node
+
+func addNode(data int) {
+	new := Node{data, nil}
+
+	if head == nil {
+		head = &new
+	} else {
+		new.next = head
+		head = &new
+	}
 
 }
 
@@ -21,23 +31,39 @@ func insertNode() {
 
 func printList() {
 
+	cur := head
+
+	for cur != nil {
+
+		fmt.Printf("%d->", cur.data)
+		cur = cur.next
+
+	}
+
 }
 
-func main() {
-
-	fmt.Println("What operation would you like to do?")
+func printMenu() {
+	fmt.Println("\nWhat operation would you like to do?")
 	fmt.Println("1. Add")
 	fmt.Println("2. Remove")
 	fmt.Println("3. Insert")
 	fmt.Println("4. Print")
 	fmt.Println("5. Quit")
+}
+
+func main() {
 
 	var option int
-
 	for option != 5 {
+		printMenu()
+		fmt.Scan(&option)
 		switch option {
 		case 1:
-			addNode()
+
+			fmt.Println("What value should be added?")
+			var new int
+			fmt.Scan(&new)
+			addNode(new)
 			fmt.Println("Node Added!")
 		case 2:
 			removeNode()
@@ -47,8 +73,11 @@ func main() {
 			fmt.Println("Node Inserted!")
 		case 4:
 			printList()
+		case 5:
+			fmt.Println("Happy Coding!")
+			break
 		default:
-			fmt.Printf("Please input a valid number:")
+			fmt.Println("Please input a valid number:")
 		}
 	}
 }
